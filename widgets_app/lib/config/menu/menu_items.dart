8c004a2f7e01d01b3a7b1fd91eda609643d9e8aa
stayandcode <moralesgarciamarc@gmail.com>
2024-12-28 18:12:26 +0100

    images added

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -49,4 +49,10 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Una serie de controles de Flutter',
       link: '/ui-controls',
       icon: Icons.car_rental_outlined),
+  
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subTitle: 'Pequeño tutorial introfductorio',
+      link: '/tutorial',
+      icon: Icons.accessible_rounded),
 ];
